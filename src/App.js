ea691d28a8741fac7ceeee626abeb7b0626f9aa5
maqiie <99504010+maqiie@users.noreply.github.com>
2023-03-03 10:41:50 +0300

    created the buttons on the home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,  } from 'react-router-dom';
 import HomePage from './views/HomePage';
 import './App.css';
 import LoginPage from './components/LoginPage.js';
@@ -13,11 +13,14 @@ import ProjectsPage from './components/ProjectsPage';
 function App() {
   return (
     <Router>
-       
-        <Route exact path="/" component={HomePage} />
+       <Routes>
+       <Route path="/" element={<HomePage />} />
+       {/* <Route exact path="/" component={HomePage} /> */}
         <Route path="/login" component={LoginPage} />
         <Route path="/signup" component={SignupPage} />
         <Route path="/projects" component={ProjectsPage} /> 
+       </Routes>
+        
       
   
   </Router>
