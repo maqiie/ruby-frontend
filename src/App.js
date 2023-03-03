befab48872736723605fe3e207644a8cb9386a41
maqiie <99504010+maqiie@users.noreply.github.com>
2023-03-03 10:37:23 +0300

    workend on home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route,  } from 'react-router-dom';
+import HomePage from './views/HomePage';
 import './App.css';
+import LoginPage from './components/LoginPage.js';
+import SignupPage from './components/SignupPage';
+import ProjectsPage from './components/ProjectsPage';
+
+
+
+
 
 function App() {
   return (
-  <h1 className='text-6xl bg-red-500'>i want this</h1>
+    <Router>
+       
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/signup" component={SignupPage} />
+        <Route path="/projects" component={ProjectsPage} /> 
+      
+  
+  </Router>
+ 
    
   );
 }
---
+++ src/views/HomePage.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Logo from '../assets/logo.png';
+
+class HomePage extends Component {
+
+    logInClick = (e) => {
+        e.preventDefault();
+        const { history } = this.props;
+        history.push('/login');
+    }
+
+    signUpClick = (e) => {
+        e.preventDefault();
+        const { history } = this.props;
+        history.push('/signup');
+    }
+
+    componentDidMount() {
+        if (localStorage.token) {
+            const { history } = this.props;
+            history.push('/projects');
+        }
+    }
+
+    render() {
+        return (
+            <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+                <div className="w-1/2">
+                    <img className="mx-auto" id='home-logo' src={Logo} alt="Logo" />
+                </div>
+                <div className="flex justify-center mt-8 space-x-4">
+                    <button onClick={this.logInClick} className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 border-b-4 border-gray-500 hover:border-gray-700 rounded">
+                        Log in
+                    </button>
+                    <button onClick={this.signUpClick} className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 border-b-4 border-gray-500 hover:border-gray-700 rounded">
+                        Sign up
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
